1bba65e49435eb2881578a3d1d9e393f6dccbdf4
Atchyut Nagbhairava <atchyutnagabhairava@gmail.com>
2018-11-20 00:16:19 +0530

    title: Added Tasks controller
    desc: Wrote all controller action methods and allowed_params method

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,19 +1,38 @@
 class TeachersController < ApplicationController
   def index
+    @teachers = Teacher.all
   end
 
   def new
+    @teacher = Teacher.new
   end
 
   def create
+    @teacher = Teacher.create(teacher_params)
+
+    redirect_to teachers_path
+  end
+
+  def show
+    @teacher = Teacher.find(params[:id])
   end
 
   def edit
-  end
+    @teacher = Teacher.find(params[:id])
 
-  def update
+    @teacher.update_attributes!(teacher_params)
   end
 
   def destroy
+    @teacher = Teacher.destroy(params[:id])
+
+    redirect_to teachers_path
+  end
+
+
+  private
+
+  def teacher_params
+    params.require(:teacher).permit(:name, :subject)
   end
 end
