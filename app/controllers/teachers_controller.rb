0b3218502e52ede38e6f00fce1bb9846e950dc32
Atchyut Nagbhairava <atchyutnagabhairava@gmail.com>
2018-11-20 01:14:40 +0530

    title: Ajaxified teacher edit and update
    desc: Added ajax and realted files for the updation of teacher record without pageload

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -9,8 +9,11 @@ class TeachersController < ApplicationController
 
   def create
     @teacher = Teacher.create(teacher_params)
-
-    redirect_to teachers_path
+    if @teacher.save
+        redirect_to @teacher, alert: "Teacher created successfully."
+    else
+        redirect_to new_teacher_path, alert: "Error creating Teacher."
+    end
   end
 
   def show
@@ -19,10 +22,16 @@ class TeachersController < ApplicationController
 
   def edit
     @teacher = Teacher.find(params[:id])
-
-    @teacher.update_attributes!(teacher_params)
   end
 
+  def update
+    @teacher = Teacher.find(params[:id])
+    @teacher.update_attributes!(teacher_params)
+
+    respond_to do |f|
+      f.js
+    end
+  end
   def destroy
     @teacher = Teacher.destroy(params[:id])
 
